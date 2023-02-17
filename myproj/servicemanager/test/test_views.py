77a9d477f32ac0e111eb0969b5aa99f2e02e09f5
Chetana <echetna@gmail.com>
2023-02-18 00:03:09 +0530

    4 more tests for view

--- myproj/servicemanager/test/test_views.py
+++ myproj/servicemanager/test/test_views.py
@@ -6,119 +6,8 @@ from mock import MagicMock
 from servicemanager.forms import TaskForm
 from servicemanager.models import Station, EmonCounter, EmonEvent
 from unittest.mock import Mock,patch, MagicMock
-import socket
-from servicemanager.views import newtask, jobhistory, StartProcess
-
-
-# class LoginTestCase(TestCase):
-
-#     def test_doLogin_InvalidLogin(self):
-#         loginClient = Client()
-#         loginUrl = reverse('dologin')
-#         response = loginClient.post(loginUrl, {'txtUsername': 'john', 'txtPassword': 'smith'})
-#         self.assertEqual(response.status_code, 200)
-#         self.assertTemplateUsed(response, "servicemanager/login.html")
-
-#     def test_doLogin_ValidLogin(self):
-#         loginClient = Client()
-#         loginUrl = reverse('dologin')
-#         response = loginClient.post(loginUrl, {'txtUsername': 'newton', 'txtPassword': 'password'})
-#         self.assertEqual(response.status_code, 302)
-#         self.assertEqual(response.content, b"id")
-#         self.assertEqual(response.url, "jobhistory")
-    
-# class IndexTestCase(TestCase):
-#     def test_Index_RedirectToLogin_IfNot_authenticated(self):
-#         indexClient = Client()
-#         indexUrl = reverse('index')
-#         response = indexClient.get(indexUrl)
-#         self.assertEqual(response.status_code, 302)
-#         self.assertEqual(response.url, "/login?next=/")
-    
-#     def test_Index_Render_Index_If_Authenticated(self):
-#         indexClient = Client()
-#         indexClient.login(username='newton',password='password')
-#         indexUrl = reverse('index')
-#         response = indexClient.get(indexUrl)
-#         self.assertEqual(response.status_code, 200)
-#         self.assertTemplateUsed(response, "servicemanager/index.html")
-
-# class TaskTestCase(TestCase):
-#     def test_Get_NewTask(self):
-#         taskClient = Client()
-#         # self.user =  User.objects.create(username='newton', password='password')
-#         taskClient.login(username='newton', password='password')       
-#         taskUrl = reverse('newtask')
-#         response = taskClient.get(taskUrl)
-#         self.assertEqual(response.status_code, 200)
-#         self.assertTemplateUsed(response, "servicemanager/newtask.html")
-#         self.assertIsNotNone(response.context['taskForm'])
-    
-#     def test_Post_NewTask_InactiveStation(self):
-#         taskClient = Client()
-#         # self.user =  User.objects.create(username='newton', password='password')
-#         self.station = Station.objects.create(StationID =1, Name='cpx-8159.fcl.com',Desc='',IsActive=False)
-#         taskClient.login(username='newton', password='password')       
-#         taskUrl = reverse('newtask')
-#         response = taskClient.post(taskUrl, data={
-#         "Stations": "cpx^cpx-8159.fcl.com",
-#         "IsDebugMode": True,
-#         "RegressionName": "Reg 21",
-#         "Tool": "Tool 1",
-#         "Platform": "cpx",
-#         "IsEmon": True,
-#         "PlatformEvent": "['INST_RETIRED_ANY']",
-#         "PlatformCounter": "['P0C3T0']",
-#         "Idea": "Idea 1",
-#         "IsUploadResults": True,
-#         "TotalIterations": 3,
-#         "Splitter": "Random",
-#         "MinImpurityDecrease": ".1",
-#         "MaxFeatures": ".2",
-#         "CreatedBy": "newton",
-#         "CreatedDate": "2023-01-19T00:35:31.178Z",
-#         "Status": "PENDING",
-#         "GUID": "b0f17f6f-3f22-4a07-9fbd-b8bc0cda7ec7",
-#         "CurrentIteration": 0,
-#         "ToolJson": ""
-#         })
-#         self.assertEqual(response.status_code, 200)
-#         self.assertTemplateUsed(response, "servicemanager/newtask.html")
-#         self.assertIsNotNone(response.context['taskForm'])
-#         self.assertEqual(response.context['message'], 'Station is already in use.')
-
-#     def test_Post_NewTask_ActiveStation(self):
-#         taskClient = Client()
-#         # self.user =  User.objects.create(username='newton', password='password')
-#         self.station = Station.objects.create(StationID =1, Name='cpx-8159.fcl.com',Desc='',IsActive=True)
-#         taskClient.login(username='newton', password='password')       
-#         taskUrl = reverse('newtask')
-#         response = taskClient.post(taskUrl, data={
-#         "Stations": "cpx^cpx-8159.fcl.com",
-#         "IsDebugMode": True,
-#         "RegressionName": "Reg 21",
-#         "Tool": "Tool 1",
-#         "Platform": "cpx",
-#         "IsEmon": True,
-#         "PlatformEvent": "['INST_RETIRED_ANY']",
-#         "PlatformCounter": "['P0C3T0']",
-#         "Idea": "Idea 1",
-#         "IsUploadResults": True,
-#         "TotalIterations": 3,
-#         "Splitter": "Random",
-#         "MinImpurityDecrease": ".1",
-#         "MaxFeatures": ".2",
-#         "CreatedBy": "newton",
-#         "CreatedDate": "2023-01-19T00:35:31.178Z",
-#         "Status": "PENDING",
-#         "GUID": "b0f17f6f-3f22-4a07-9fbd-b8bc0cda7ec7",
-#         "CurrentIteration": 0,
-#         "ToolJson": ""
-#         })
-#         self.assertEqual(response.status_code, 302)
-#         self.assertEqual(response.url, "jobhistory")
-
-
+from django.db.models import Q
+from servicemanager.views import newtask, jobhistory, StartProcess, ShowToolJson, ShowJson, SendJson, filterData
 
 
 class NewTaskViewTest(TestCase):
@@ -218,5 +107,43 @@ class ProcessTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.content.__contains__(b"error"))
 
+class ShowToolJsonTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = MagicMock(username="Test")
+        self.guid = uuid.uuid1()
+    
+
+    @patch("django.contrib.auth.decorators.user_passes_test")
+    def test_showtooljson(self, mock_ldap_auth):
+        mock_ldap_auth.return_value = True
+        request = self.factory.get("ShowToolJson/", data={'filename': 'Tool1.json'})
+        request.user = self.user
+        response = ShowToolJson(request, 'Tool1.json')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.content.__contains__(b"local-cpu-00"))
+    
+    @patch("django.contrib.auth.decorators.user_passes_test")
+    def test_showjson(self, mock_ldap_auth):
+        mock_ldap_auth.return_value = True
+        request = self.factory.get("ShowJson")
+        request.user = self.user
+        response = ShowJson(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.content.__contains__(b"Regression 17"))
+    
 
 
+    # @patch("servicemanager.models.TaskIteration.objects.all", return_value=[{'id':1, 'TaskID':1, 'GUID':'b0f17f6f-3f22-4a07-9fbd-b8bc0cda7ec7', 'Iteration':3, 'JSONData':''}, {'id':2, 'TaskID':2, 'GUID':'b0f17f6f-3f22-4a07-9fbd-b8bc0cday657', 'Iteration':0, 'JSONData':''}])
+    # @patch("servicemanager.models.Task.objects.filter", return_value=[{'TaskID':1, 'Station':'cpx-i5-47l', 'IsDebugMode':'true', 'RegressionName':'Reg 1','Tool':'Tool 1', 'ToolEvent':'["1","3"]', 'Platform':'Platform 1','Idea':'Idea 1'}, {'TaskID':2, 'Station':'cpx-i7-47l', 'IsDebugMode':'true', 'RegressionName':'Reg 2','Tool':'Tool 2', 'ToolEvent':'["1","4"]', 'Platform':'Platform 2','Idea':'Idea 2'}])
+    # @patch("django.contrib.auth.decorators.user_passes_test")
+    # def test_filterdata(self, mock_ldap_auth, mock_task, mock_taskIteration):
+    #     mock_ldap_auth.return_value = True
+    #     request = self.factory.post("filterData", data={ 'txtFromDate':'1/22/2023', 'txtToDate':'' })
+    #     request.user = self.user
+    #     mock_task.return_value = [MagicMock(TaskID=1, GUID=self.guid,Status='PENDING',Station='Station 1'),MagicMock(TaskID=2, GUID=uuid.uuid1(),Status='COMPLETED',Station='Station 2')]
+    #     mock_task.return_value[0].Status = "PENDING"
+    #     mock_taskIteration.return_value = Q[MagicMock(id=1, TaskID=1,GUID='b0f17f6f-3f22-4a07-9fbd-b8bc0cda7ec7',Iteration=3, JSONData=''), MagicMock(id=2,TaskID=2,GUID='b0f17f6f-3f22-4a07-9fbd-b8bc0cday657', Iteration=0,JSONData='')]
+    #     mock_taskIteration.return_value.order_by = Q[MagicMock(id=1, TaskID=1,GUID='b0f17f6f-3f22-4a07-9fbd-b8bc0cda7ec7',Iteration=3, JSONData=''), MagicMock(id=2,TaskID=2,GUID='b0f17f6f-3f22-4a07-9fbd-b8bc0cday657', Iteration=0,JSONData='')]
+    #     response = filterData(request)
+    #     self.assertEqual(response.status_code, 200)
